b8a2adb72e6d06654b219639cd13b36e2a48e0b8
Ekaterina Pankratova <pankratova_e@mail.ru>
2021-05-21 12:31:07 +0300

    lesson 3, home work: vuex, pagination

--- test_project/src/App.vue
+++ test_project/src/App.vue
@@ -3,8 +3,9 @@
 <header><h2>My personal costs</h2></header>
 <main>
 <button @click="clicked">Add new cost</button>
-<PaymentsForm @add="onDataAdded" v-if="formIsVisible" />
-<PaymentsList :items="paymentsList" />
+<PaymentsForm  v-if="formIsVisible" />
+<PaymentsList />
+<Pagination />
 </main>
 </div>
 </template>
@@ -12,150 +13,38 @@
 <script>
 import PaymentsList from "./components/PaymentsList.vue";
 import PaymentsForm from "./components/PaymentsForm.vue";
+import Pagination from "./components/Pagination.vue";
+
+import { mapActions } from 'vuex';
+
 
 export default {
-name: "App",
-data() {
-return {
-paymentsList: [
-{
-date: '13.05.2021',
-category: 'Education',
-price: 1,
-},
-{
-date: '12.05.2021',
-category: 'Education',
-price: 2,
-},
-{
-date: '11.05.2021',
-category: 'Education',
-price: 3,
-},
-{
-date: '10.05.2021',
-category: 'Education',
-price: 4,
-},
-{
-date: '13.05.2021',
-category: 'Education',
-price: 5,
-},
-{
-date: '12.05.2021',
-category: 'Education',
-price: 6,
-},
-{
-date: '11.05.2021',
-category: 'Education',
-price: 7,
-},
-{
-date: '10.05.2021',
-category: 'Education',
-price: 8,
-},
-{
-date: '13.05.2021',
-category: 'Education',
-price: 9,
-},
-{
-date: '12.05.2021',
-category: 'Education',
-price: 10,
-},
-{
-date: '11.05.2021',
-category: 'Education',
-price: 11,
-},
-{
-date: '10.05.2021',
-category: 'Education',
-price: 12,
-},
-{
-date: '13.05.2021',
-category: 'Education',
-price: 13,
-},
-{
-date: '12.05.2021',
-category: 'Education',
-price: 14,
-},
-{
-date: '11.05.2021',
-category: 'Education',
-price: 15,
-},
-{
-date: '10.05.2021',
-category: 'Education',
-price: 16,
-},
-{
-date: '13.05.2021',
-category: 'Education',
-price: 17,
-},
-{
-date: '12.05.2021',
-category: 'Education',
-price: 18,
-},
-{
-date: '11.05.2021',
-category: 'Education',
-price: 19,
-},
-{
-date: '10.05.2021',
-category: 'Education',
-price: 20,
-},
-{
-date: '13.05.2021',
-category: 'Education',
-price: 21,
-},
-{
-date: '12.05.2021',
-category: 'Education',
-price: 22,
-},
-{
-date: '11.05.2021',
-category: 'Education',
-price: 23,
-},
-{
-date: '10.05.2021',
-category: 'Education',
-price: 24,
-},
-],
-formIsVisible: false,
-}
-},
-methods: {
-onDataAdded(data) {
-this.paymentsList.push(data)
-},
-clicked() {
-this.formIsVisible = !this.formIsVisible;
-console.log(this.formIsVisible);
-},
-},
-components: {
-PaymentsList,
-PaymentsForm,
-
-},
+  name: "App",
+  data() {
+    return {
+    
+    formIsVisible: false,
+  }
+  },
+  methods: {
+    ...mapActions([
+      'fetchData', 'fetchCategoriesList'
+    ]),
+    
+    clicked() {
+      this.formIsVisible = !this.formIsVisible;
+      console.log(this.formIsVisible);
+    },
+  },
+  components: {
+    PaymentsList,
+    PaymentsForm,
+    Pagination
+  },
+  mounted() {
+    this.fetchData();
+    this.fetchCategoriesList();
+  }
 };
 </script>
  
